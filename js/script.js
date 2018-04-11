4cd9815fd74c55dd21b432855003dbfdfeee3f8e
shireesha12 <karra.shireesha1220@gmail.com>
2018-04-10 20:01:43 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+/*eslint-env browser*/
+/*var Car = function (vin) {
+    "use strict";
+};
+var camaro = new Car("476478789");
+window.console.log(camaro);*/
+
+/*var Car = function (vin, make, model) {
+    "use strict";
+    this.vin = vin;
+    this.make = make;
+    this.model = model;
+    return "you just created a new " + this.make + " " + this.model;
+};
+var camaro = new Car("476478789", "Chevy", "Camaro");
+window.console.log(camaro);*/
+
+/*var Car = function (make, model) {
+    "use strict";
+    this.make = make;
+    this.model = model;
+    
+};
+Car.prototype.start = function () {
+  "use strict";
+    window.console.log("The " + this.make + " " + this.model + " has started");
+};
+var camaro = new Car("Chevy", "Camaro");
+var corvette = new Car("Chevy", "Corvette");
+//window.console.log("you just created a new " + camaro.make + " " + camaro.model);
+//window.console.log("you just created a new " + corvette.make + " " + corvette.model);
+window.console.log(camaro);
+camaro.start();
+corvette.start();*/
+
+/*var Car = function (make, model) {
+    "use strict";
+    var make = make;
+    var model = model;
+    var showMake = function () {
+        "use strict";
+        window.console.log("You own a " + make + " " + model);
+    };
+    this.displayCarInfo = function () {
+        "use strict";
+        showMake();
+    };
+    
+};
+
+var camaro = new Car("Chevy", "Camaro");
+var corvette = new Car("Chevy", "Corvette");
+
+camaro.displayCarInfo();
+corvette.displayCarInfo();
+camaro.showMake();*/
+
